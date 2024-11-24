b1875b2c2299c98d2ea111a73e10cb3e8c7b6396
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2024-11-24 10:59:02 +0330

    Initial Commit

---
+++ CacheManagerMvvm/CacheManagerMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheManagerMvvmApp.swift
+//  CacheManagerMvvm
+//
+//  Created by Hamid on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CacheManagerMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
